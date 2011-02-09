09acf85757d508ed78aef1c410df11722f523b12
Lucas Chan <lucas-github.com@lucaschan.com>
2011-02-09 15:34:39 +1100

    Added 503 headers and redirects.

--- index.php
+++ index.php
@@ -1,4 +1,13 @@
+<?php
 
+header( 'HTTP/1.1 503 Service Unavailable' );
+header( 'Expires: Mon, 26 Jul 1997 05:00:00 GMT' );
+header( 'Last-Modified: ' . gmdate( 'D, d M Y H:i:s' ) . ' GMT' );
+header( 'Cache-Control: no-store, no-cache, must-revalidate' );
+header( 'Cache-Control: post-check=0, pre-check=0', false );
+header( 'Pragma: no-cache' );
+
+?>
 <!DOCTYPE html>
 <html lang="en">
 	<head>
